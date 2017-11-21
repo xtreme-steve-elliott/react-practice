f7b51cce21d03e1d246f5740f99a9acffbc2e94d
xtreme-steve-elliott <selliott@pivotal.io>
2017-11-21 15:00:49 -0500

    Update index.js

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -173,17 +173,34 @@ function boardDiff(curr, prev) {
   return ret;
 }
 
+function genLines(itemCount) {
+  const dimen = Math.sqrt(itemCount);
+  let ret = [];
+  let diag = [];
+  let revDiag = [];
+  Array.apply(0, Array(dimen)).map((x, i) => {
+    let row = [];
+    let col = [];
+    Array.apply(0, Array(dimen)).map((y, j) => {
+      row.push((i * dimen) + j);
+      col.push(i + (j * dimen));
+      if (i == j) {
+        diag.push(i + (j * dimen));
+      }
+      if (dimen - 1 - i == j) {
+        revDiag.push((i * dimen) + j);
+      }
+    });
+    ret.push(row);
+    ret.push(col);
+  });
+  ret.push(diag);
+  ret.push(revDiag);
+  return ret;
+}
+
 function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
+  const lines = genLines(squares.length);
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
